7c1b8607451126079b15ca60566ea377ad6b87a8
SufiyaSarwath <sufiyasarwath23@gmail.com>
2024-12-17 19:46:09 +0530

    Replaced repository contents with new folder

--- configs.py
+++ configs.py
@@ -1,32 +1,10 @@
-"""def Model_config():
-    config = {}
-    config['batch_size'] = 32
-    config['num_workers'] = 8                 # number of data loading workers
-    config['epochs'] = 200                    # number of total epochs to run
-    config['lr'] = 1e-4                       # initial learning rate
-    config['num_classes'] = 86
-    
-    # Molormer
-    config['n_layers'] = 3
-    config['num_heads'] = 8
 
-    config['hidden_dim'] = 256
-    config['fnn_dim'] = 256
-
-    config['input_dropout_rate'] = 0
-    config['encoder_dropout_rate'] = 0
-    config['attention_dropout_rate'] = 0
-    
-    config['flatten_dim'] = 2048              # molormer:2048, transformer:8192， no_distil：8192， no_share:2048
-    
-    return config
-"""
 
 def Model_config():
     config = {}
     config['batch_size'] = 32
-    config['num_workers'] = 8                 # number of data loading workers
-    config['epochs'] = 200                    # number of total epochs to run
+    config['num_workers'] = 8                 # (32)number of data loading workers
+    config['epochs'] = 50                    # number of total epochs to run
     config['lr'] = 1e-4                       # initial learning rate
     config['num_classes'] = 86
     
